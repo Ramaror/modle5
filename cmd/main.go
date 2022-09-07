5945a58c5776a7dff89484c1a88c7fb53885716a
Ramaror <mail.the.ramzan@mail.ru>
2022-09-07 22:04:29 +0300

    5.01

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	go spinner(100 * time.Millisecond)
+	n := 44
+	fibN := fib(n)
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+	go spinner(100 * time.Millisecond)
+	d := 45
+	fibD := fib(d)
+	fmt.Printf("\rFibonacci(%d) = %d\n", d, fibD)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
 }
